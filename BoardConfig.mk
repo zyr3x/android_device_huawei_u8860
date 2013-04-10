cce5223052d9be2c856c093daf454e0c8cf5857a
airk000 <airk908@gmail.com>
2013-04-10 21:24:09 +0800

    Add limitation for msm7x30-common
    Change-Id: Icaff662ec9f10336b23f609efd69f09b436c7fcb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,8 @@
 -include vendor/huawei/u8860/BoardConfigVendor.mk
 -include device/huawei/msm7x30-common/BoardConfigCommon.mk
 
+TARGET_MSM7X30_COMMON := true
+
 TARGET_BOOTLOADER_BOARD_NAME := u8860
 TARGET_OTA_ASSERT_DEVICE := u8860,U8860,hwu8860
 
