6ddbd246343c7e3bf8b281fa3b3090add69c9760
zyr3x <izzzum@gmail.com>
2013-04-28 10:55:39 +0300

    Add fix cache

--- device_u8860.mk
+++ device_u8860.mk
@@ -6,6 +6,9 @@ DEVICE_PACKAGE_OVERLAYS += device/huawei/u8860/overlay
 
 LOCAL_PATH := device/huawei/u8860
 
+PRODUCT_COPY_FILES += \
+	device/huawei/u8860/prebuilt/etc/init.d/02fixcache:system/etc/init.d/02fixcache
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
